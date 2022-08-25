18d5db1ec60a49bde14ed8b248a86edc1bb6e7c1
okolipraiz <okolipraiz@gmail.com>
2022-08-25 03:00:51 +0100

    file update

---
+++ db.js
@@ -0,0 +1,11 @@
+// const items = require('./data/items');
+const users = require('./data/users');
+const tags = require('./data/tags');
+const comments = require('./data/comments');
+
+module.exports = {
+  items,
+  users,
+  tags,
+  comments
+}
