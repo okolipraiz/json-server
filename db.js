82e2ad8fd90b0463a0d136985ba6dad9f92cb25b
okolipraiz <okolipraiz@gmail.com>
2022-08-25 03:13:59 +0100

    file fix

--- db.js
+++ db.js
@@ -1,10 +1,8 @@
-// const items = require('./data/items');
 const users = require('./data/users');
 const tags = require('./data/tags');
 const comments = require('./data/comments');
 
 module.exports = {
-  items,
   users,
   tags,
   comments
