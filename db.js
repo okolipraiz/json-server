c12fb44d1d75b51d8a5ddce53cead977166e0fe3
okolipraiz <okolipraiz@gmail.com>
2022-08-26 00:23:31 +0100

    file update

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const users = require('./data/users');
 const tags = require('./data/tags');
 const comments = require('./data/comments');
+const currency = require("./data/currency");
 
 module.exports = {
   users,
   tags,
-  comments
+  comments,
+  currency
 }
