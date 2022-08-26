0fcec781f01d3567d295ee22920248d8efd24288
okolipraiz <okolipraiz@gmail.com>
2022-08-26 02:08:01 +0100

    End point complete

--- db.js
+++ db.js
@@ -1,11 +1,17 @@
-const users = require('./data/users');
-const tags = require('./data/tags');
-const comments = require('./data/comments');
-const currency = require("./data/currency");
+const account = require('./data/account');
+const balance = require('./data/balance');
+const budget = require('./data/budget');
+const earnings = require('./data/earnings');
+const expense = require('./data/expense');
+const notes = require('./data/notes');
+const savings = require("./data/savibngs");
 
 module.exports = {
-  users,
-  tags,
-  comments,
-  currency
+  account,
+  balance,
+  budget,
+  earnings,
+  expense,
+  notes,
+  savings
 }
